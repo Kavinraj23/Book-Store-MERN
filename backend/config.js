2452cab667c13bfe4c3c2169facd4fd4b1493764
Kavin Raj <kavin.raj@outlook.com>
2025-01-03 11:28:22 -0600

    removed config.js from version control

--- backend/config.js
+++
@@ -1,4 +0,0 @@
-export const PORT = 5555;
-//Gb60pFemqAV9koMc
-export const mongoDBURL = 
- 'mongodb+srv://kavinraj:Gb60pFemqAV9koMc@book-store-mern.y6va7.mongodb.net/?retryWrites=true&w=majority&appName=Book-Store-MERN'
