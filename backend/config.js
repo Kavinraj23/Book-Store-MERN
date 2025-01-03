34132c46161acf54df7b51e6accfaf0491939d44
Kavin Raj <kavin.raj@outlook.com>
2025-01-03 11:23:19 -0600

    initial commit

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+//Gb60pFemqAV9koMc
+export const mongoDBURL = 
+ 'mongodb+srv://kavinraj:Gb60pFemqAV9koMc@book-store-mern.y6va7.mongodb.net/?retryWrites=true&w=majority&appName=Book-Store-MERN'
---
+++ backend/index.js
@@ -0,0 +1,43 @@
+import express, { request } from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
+import booksRoute from './routes/booksRoute.js';
+import cors from 'cors';
+
+export const app = express();
+
+// Middleware for parsing request body
+app.use(express.json());
+
+// Middleware for handling CORS POLICY
+// Option 1: Allow all origins with default of cors(*)
+app.use(cors());
+// Option 2 : Allow custom origins
+// app.use(
+//     cors({
+//         origin: 'http://localhost:3000',
+//         methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//         allowedHeaders: ['Content-Type'],
+//     })
+// )
+
+app.get('/', (request, response) => {
+    console.log(request)
+    return response.status(234).send("mern stack project")
+});
+
+app.use('/books', booksRoute)
+
+mongoose
+    .connect(mongoDBURL)
+    .then(() => {
+        console.log('App connected to database');
+        app.listen(PORT, () => {
+            console.log(`App is listening to port: ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+
